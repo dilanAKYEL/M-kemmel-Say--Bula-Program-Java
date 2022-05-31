db9f624621b71e6b9c078fe26e44fce08c923fd6
dilanAKYEL <dln.1087.yldrm@outlook.com>
2022-05-31 21:46:17 +0300

    Mükemmel Sayı Bulan Program Tamamlandı.

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[]args){
+        int sayi , carpanToplam=0;
+        Scanner sc =new Scanner(System.in);
+        System.out.print("Bir sayı giriniz:");
+        sayi=sc.nextInt();
+
+        for (int i=1; i<sayi; i++){
+            if (sayi% i==0){
+                carpanToplam+=i;
+
+            }
+        }
+   if (carpanToplam==sayi){
+       System.out.println(sayi+"Mükemmel sayıdır.");
+   }else {
+       System.out.println(sayi+"Mükemmel sayı değildir.");
+   }
+
+    }
+}
